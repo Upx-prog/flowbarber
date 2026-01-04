2b56d0439d8149aca030d5670eae103fde0f85fc
Upx-prog <gr.espirito@gmail.com>
2026-01-03 21:26:20 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'pixflow-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
